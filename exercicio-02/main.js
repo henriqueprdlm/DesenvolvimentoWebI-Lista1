9785c3c520ed585ed138610654f6503cc025792c
Henrique Pieri de Lima <henrique070425@gmail.com>
2024-09-22 23:47:02 -0300

    final

--- exercicio-02/main.js
+++ exercicio-02/main.js
@@ -1,38 +1,74 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const taskInput = document.getElementById('task-input');
+const listContainer = document.getElementById('list-container');
+const searchInput = document.getElementById('search-input');
+const body = document.body;
+const toDoList = document.querySelector('.to-do-list');
+const addTaskBox = document.querySelector('.add-task');
+const imgTheme = document.getElementById('img-theme');
 
 function addTask() {
-    if (inputBox.value === '') {
-        alert("You must write something!");
+    if (taskInput.value === '') {
+        alert('You must write something!');
     } else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        let li = document.createElement('li');
+        li.innerHTML = taskInput.value;
         listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+        let removeButton = document.createElement('span');
+        removeButton.innerHTML = '\u00d7';
+        li.appendChild(removeButton);
     }
 
-    inputBox.value = "";
+    taskInput.value = '';
     saveData();
 }
 
-listContainer.addEventListener("click", function (e) {
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
+listContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
         saveData();
-    } else if (e.target.tagName === "SPAN") {
+    } else if (e.target.tagName === 'SPAN') {
         e.target.parentElement.remove();
         saveData();
     }
 }, false);
 
 function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML);
+    localStorage.setItem('data', listContainer.innerHTML);
 }
 
 function showTasks() {
-    listContainer.innerHTML = localStorage.getItem("data");
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+
+function filterTasks() {
+    const filter = searchInput.value.toLowerCase();
+    const tasks = listContainer.getElementsByTagName('li');
+
+    for (let i = 0; i < tasks.length; i++) {
+        const taskText = tasks[i].innerText || tasks[i].textContent;
+        tasks[i].style.display = taskText.toLowerCase().includes(filter) ? '' : 'none';
+    }
+}
+
+function changeTheme() {
+    body.classList.toggle('dark');
+    toDoList.classList.toggle('dark');
+    addTaskBox.classList.toggle('dark');
+    searchInput.classList.toggle('dark');
+    body.classList.contains('dark') ? imgTheme.src = 'img/theme-light.png' : imgTheme.src = 'img/theme-dark.png';
+    localStorage.setItem('theme', body.classList.contains('dark') ? 'dark': 'light');
+}
+
+function loadTheme() {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+        body.classList.add('dark');
+        toDoList.classList.add('dark');
+        addTaskBox.classList.add('dark');
+        searchInput.classList.add('dark');
+        imgTheme.src = 'img/theme-light.png';
+    }
 }
 
 showTasks();
+loadTheme();
---
+++ exercicio-04/main.js
@@ -0,0 +1,145 @@
+const video = document.querySelector('video');
+const galeria = document.querySelector('div.galeria');
+const tituloFoto = document.getElementById('tituloFoto');
+const descricaoFoto = document.getElementById('descricaoFoto');
+
+navigator.mediaDevices.getUserMedia({video: true})
+.then(stream => {
+    video.srcObject = stream;
+    video.play();
+})
+.catch(error => {
+    console.log(error);
+})
+
+function obterLocalizacao() {
+    return new Promise((resolve, reject) => {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    resolve({ latitude, longitude });
+                },
+                (error) => {
+                    reject(error);
+                }
+            );
+        } else {
+            reject(new Error('Geolocalização não disponível'));
+        }
+    });
+}
+
+async function tirarFoto() {
+    if (tituloFoto.value === '') {
+        alert('Você deve dar um título à foto!');
+        return;
+    } else {
+        let divImg = document.createElement('div');
+        divImg.classList.add('row');
+        galeria.appendChild(divImg);
+
+        let divColumn1 = document.createElement('div');
+        divColumn1.classList.add('column');
+        divImg.appendChild(divColumn1);
+
+        let canvas = document.createElement('canvas');
+        divColumn1.appendChild(canvas);
+        canvas.height = video.videoHeight;
+        canvas.width = video.videoWidth;
+        let context = canvas.getContext('2d');
+        context.drawImage(video, 0, 0);
+        
+        let imagemBase64 = canvas.toDataURL();
+
+        let divColumn2 = document.createElement('div');
+        divColumn2.classList.add('column');
+        divImg.appendChild(divColumn2);
+
+        let divElementId = document.createElement('div');
+        divElementId.classList.add('element');
+        divColumn2.appendChild(divElementId);
+        let titleId = document.createElement('h4');
+        titleId.innerHTML = 'ID:';
+        divElementId.appendChild(titleId);
+        let textId = document.createElement('p');
+        let idAtual = localStorage.getItem('id') ? parseInt(localStorage.getItem('id')) : 1;
+        textId.innerHTML = idAtual;
+        localStorage.setItem('id', idAtual+1);
+        divElementId.appendChild(textId);
+
+        let divElementTitulo = document.createElement('div');
+        divElementTitulo.classList.add('element');
+        divColumn2.appendChild(divElementTitulo);
+        let titleTitulo = document.createElement('h4');
+        titleTitulo.innerHTML = 'Título:';
+        divElementTitulo.appendChild(titleTitulo);
+        let textTitulo = document.createElement('p');
+        textTitulo.innerHTML = tituloFoto.value;
+        divElementTitulo.appendChild(textTitulo);
+
+        if (descricaoFoto.value) {
+            let divElementDescricao = document.createElement('div');
+            divElementDescricao.classList.add('element');
+            divColumn2.appendChild(divElementDescricao);
+            let titleDescricao = document.createElement('h4');
+            titleDescricao.innerHTML = 'Descrição:';
+            divElementDescricao.appendChild(titleDescricao);
+            let textDescricao = document.createElement('p');
+            textDescricao.innerHTML = descricaoFoto.value;
+            divElementDescricao.appendChild(textDescricao);
+        }
+
+        let divElementData = document.createElement('div');
+        divElementData.classList.add('element');
+        divColumn2.appendChild(divElementData);
+        let titleData = document.createElement('h4');
+        titleData.innerHTML = 'Data:';
+        divElementData.appendChild(titleData);
+        let textData = document.createElement('p');
+        let agora = new Date();
+        textData.innerHTML = (agora.getDate() + '/' + agora.getMonth() + '/' + agora.getFullYear());
+        divElementData.appendChild(textData);
+
+        try {
+            const { latitude, longitude } = await obterLocalizacao(); // Espera a localização
+            let divElementLocalizacao = document.createElement('div');
+            divElementLocalizacao.classList.add('element');
+            divColumn2.appendChild(divElementLocalizacao);
+            
+            let titleLocalizacao = document.createElement('h4');
+            titleLocalizacao.innerHTML = 'Localização:';
+            divElementLocalizacao.appendChild(titleLocalizacao);
+            
+            let textLocalizacao = document.createElement('p');
+            textLocalizacao.innerHTML = `(${latitude}, ${longitude})`;
+            divElementLocalizacao.appendChild(textLocalizacao);
+        } catch (error) {
+            console.error('Erro ao obter localização:', error);
+        }
+
+        let botaoDeletar = document.createElement('button');
+        botaoDeletar.classList.add('btnDeletar');
+        botaoDeletar.innerHTML = 'Deletar';
+        divColumn2.appendChild(botaoDeletar);
+
+        botaoDeletar.addEventListener('click', () => {
+            galeria.removeChild(divImg); 
+        });
+
+        tituloFoto.value = '';
+        descricaoFoto.value = '';
+
+        salvarDados();
+    }
+}
+
+function salvarDados() {
+    
+}
+
+function carregarDados() {
+    
+}
+
+carregarDados();
